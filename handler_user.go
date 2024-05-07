9d34e0b54cebbf6170bdec3a2cde3d830559639d
HH00254 <105815210+HH00254@users.noreply.github.com>
2024-05-07 17:21:30 -0500

    Minor Updates to the server

--- handler_user.go
+++ handler_user.go
@@ -2,22 +2,38 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/ahochbaum-rcg/rssagg/internal/database"
+	"github.com/google/uuid"
 )
 
 func (apiCon *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Request) {
 	type parameters struct {
-		Name string `name`
+		Name string `json:"name"`
 	}
 
-	decoder := json.NewDecoder(r.body)
+	decoder := json.NewDecoder(r.Body)
 
 	param := parameters{}
 	err := decoder.Decode(&param)
 	if err != nil {
-		respongWithError(w, 400, "Failed to decode JSON body")
+		respongWithError(w, 400, fmt.Sprintf("Error parsing JOSN: %v", err))
 		return
 	}
 
-	responWithJSON(w, 200, struct{}{})
+	user, err := apiCon.DB.CreateUser(r.Context(), database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now().UTC(),
+		UpdatedAt: time.Now().UTC(),
+		Name:      param.Name,
+	})
+	if err != nil {
+		respongWithError(w, 400, fmt.Sprintf("Couldn't create user: %v", err))
+		return
+	}
+
+	responWithJSON(w, 200, databaseUserToUser(user))
 }
